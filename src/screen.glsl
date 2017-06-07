5f94d4e90efc525dd84b393d2bd5ba68124986b9
Graham <graham.gibbons@gmail.com>
2017-06-07 14:22:56 -0700

    Gamma correction

--- src/screen.glsl
+++ src/screen.glsl
@@ -5,5 +5,5 @@ uniform sampler2D texture;
 
 void main() {
 	vec4 result = texture2D(texture, uv * 0.5 - 0.5);
-	gl_FragColor = vec4(result.xyz / result.w, 1.0);
+	gl_FragColor = vec4(pow(result.xyz / result.w, vec3(1.0 / 2.2)), 1.0);
 }
