bc5b658d5bce0c2738bbb2ebb253e046e77fed8f
Graham <graham.gibbons@gmail.com>
2017-06-08 10:18:40 -0700

    Decent test case

--- index.ts
+++ index.ts
@@ -1,31 +1,34 @@
 import {createViewer} from "./src/viewer";
 import {material} from "./src/material";
 import {createScene} from "./src/scene";
-import {plane, translate, unitSphere} from "./src/shape";
+import {blend, plane, rotateY, scale, translate, twistZ, unitCylinder, unitSphere} from "./src/shape";
 import {value} from "./src/expression";
 import {entity} from "./src/entity";
 
 const scene = createScene([
 	entity(
-		plane(value(0, 0, 1), value(2.0)),
+		plane(value(0, 0, 1), value(3.0)),
 		material({
 			color: `mod(floor(p.x) + floor(p.y), 2.0) * vec3(0.9) + vec3(0.1)`,
 			smoothness: value(0.5)
 		})),
 	entity(
-		unitSphere(),
-		material({
-			color: value(0.9, 0.5, 0.5),
-			transmittance: value(0.9),
-			smoothness: value(0.9),
-			refraction: value(1.4)
-		})),
-	entity(
-		translate(value(4, 1, 4), unitSphere()),
+		scale(value(100), unitSphere()),
 		material({
 			color: value(0, 0, 0),
-			emissivity: value(20, 20, 20)
-		}))
+			emissivity: value(1.2, 1.2, 1.2)
+		})),
+	entity(
+		rotateY(value(Math.PI / 2),
+			blend(8.0,
+				unitSphere(),
+				scale(value(0.1),
+					twistZ(value(Math.PI / 8),
+						translate(value(4, 0, 0), unitCylinder()))))),
+		material({
+			color: value(0.9, 0.5, 0.5),
+			smoothness: value(0.95),
+		})),
 ]);
 
 createViewer(document.body, scene);
