33f57bf35a69db8b570f5d608b331b8f404698bf
Graham <graham.gibbons@gmail.com>
2017-06-14 10:14:02 -0700

    Cleanup

--- src/camera.ts
+++ src/camera.ts
@@ -20,21 +20,25 @@ export function camera(values?: {
 		eye: values.eye || value(0, 0, -1),
 		target: values.target || value(0, 0, 0),
 		up: values.up || value(0, 1, 0),
-		fieldOfView: values.fieldOfView || value(120 / 180.0 * Math.PI),
-		aperture: values.aperture || value(0.1)
+		fieldOfView: values.fieldOfView || value(90 / 180.0 * Math.PI),
+		aperture: values.aperture || value(0.0)
 	};
 }
 
 export function mouseCamera(values?: {
 								target?: Expression,
 								distance?: Expression,
-								focalDistance?: Expression,
+								up?: Expression,
+								fieldOfView?: Expression,
 								aperture?: Expression
 							}): Camera {
 	values = values || {};
 	values.target = values.target || value(0, 0, 0);
 	return camera({
 		target: values.target,
-		eye: `${values.target} + ${values.distance || value(1)}.x * spherical(mouse * vec2(PI, 0.5 * PI) + vec2(0, 0.5 * PI))`
+		eye: `${values.target} + ${values.distance || value(1)}.x * spherical(mouse * vec2(PI, 0.5 * PI) + vec2(0, 0.5 * PI))`,
+		up: values.up,
+		fieldOfView: values.fieldOfView,
+		aperture: values.aperture
 	});
 }
