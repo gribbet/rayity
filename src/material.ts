c64bf049e99c07f16d0ba9d4ba9818cde745aaa5
Graham <graham.gibbons@gmail.com>
2017-07-09 16:55:28 -0700

    Add light material

--- src/material.ts
+++ src/material.ts
@@ -1,4 +1,4 @@
-import {Expression, value} from "./expression";
+import { Expression, value } from "./expression";
 
 export type Material = {
 	transmittance: Expression,
@@ -9,15 +9,14 @@ export type Material = {
 	emissivity: Expression
 }
 
-
 export function material(values?: {
-							 transmittance?: Expression,
-							 smoothness?: Expression,
-							 refraction?: Expression,
-							 scatter?: Expression,
-							 color?: Expression,
-							 emissivity?: Expression
-						 }) {
+	transmittance?: Expression,
+	smoothness?: Expression,
+	refraction?: Expression,
+	scatter?: Expression,
+	color?: Expression,
+	emissivity?: Expression
+}) {
 	return Object.assign({
 		transmittance: value(0),
 		smoothness: value(0),
@@ -26,4 +25,19 @@ export function material(values?: {
 		color: value(1, 1, 1),
 		emissivity: value(0, 0, 0)
 	}, values || {});
+}
+
+export function spotlightMaterial(options: {
+	direction?: Expression,
+	color?: Expression,
+	spread?: Expression
+}): Material {
+	options = options || {};
+	let direction = options.direction || value(0, 1, 0);
+	let color = options.color || value(1, 1, 1);
+	let spread = options.spread || value(1);
+	return material({
+		color: value(0),
+		emissivity: `${color} / ${spread}.x * pow(dot(normalize(p), normalize(${direction})), 1.0 / ${spread}.x - 1.0)`
+	});
 }
