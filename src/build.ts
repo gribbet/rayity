9c889dc4994e20b3c4d073b836c44b5783979a81
Graham <graham.gibbons@gmail.com>
2017-07-27 13:05:51 -0700

    Cleanup

--- src/build.ts
+++ src/build.ts
@@ -7,9 +7,11 @@ import { Shape } from './shape';
 function buildShape(shape: Shape) {
 	return dependencies(shape)
 		.map(shape => `
-				float shape${shape.id}(vec3 p) {
-					${shape.body}
-				}`)
+
+float shape${shape.id}(vec3 p) {
+	${shape.body}
+}`)
+
 		.reduce((a, b) => a + "\n" + b, "");
 }
 
@@ -28,50 +30,52 @@ function buildModel(
 		cheapNormals: boolean
 	}): Code {
 	let code = `
-		${buildShape(model.shape)}
+
+${buildShape(model.shape)}
 	
-		float distance${model.id}(vec3 p) {
-			return ${model.shape.call(variable("p"))};
-		}
-		
-		Material material${model.id}(vec3 p, vec3 n, vec3 d) {
-			Material m;
-			m.transmittance = ${model.material.transmittance}.x;
-			m.smoothness = ${model.material.smoothness}.x;
-			m.refraction = ${model.material.refraction}.x;
-			m.scatter = ${model.material.scatter}.x;
-			m.color = ${model.material.color};
-			m.emissivity = ${model.material.emissivity};
-			return m;
-		}
-		`;
+float distance${model.id}(vec3 p) {
+	return ${model.shape.call(variable("p"))};
+}
+
+Material material${model.id}(vec3 p, vec3 n, vec3 d) {
+	Material m;
+	m.transmittance = ${model.material.transmittance}.x;
+	m.smoothness = ${model.material.smoothness}.x;
+	m.refraction = ${model.material.refraction}.x;
+	m.scatter = ${model.material.scatter}.x;
+	m.color = ${model.material.color};
+	m.emissivity = ${model.material.emissivity};
+	return m;
+}`;
+
 	if (options.cheapNormals)
 		code += `
-		vec3 normal${model.id}(vec3 p) {
-			const vec3 a = vec3( 1, 1, 1) / sqrt(3.0);
-			const vec3 b = vec3( 1,-1,-1) / sqrt(3.0);
-			const vec3 c = vec3(-1, 1,-1) / sqrt(3.0);
-			const vec3 d = vec3(-1,-1, 1) / sqrt(3.0);
-			float z = distance${model.id}(p);
-			return normalize(
-				a * (distance${model.id}(p + a * epsilon) - z) + 
-				b * (distance${model.id}(p + b * epsilon) - z) +
-				c * (distance${model.id}(p + c * epsilon) - z) +
-				d * (distance${model.id}(p + d * epsilon) - z));
-		}
-		`;
+
+vec3 normal${model.id}(vec3 p) {
+	const vec3 a = vec3( 1, 1, 1) / sqrt(3.0);
+	const vec3 b = vec3( 1,-1,-1) / sqrt(3.0);
+	const vec3 c = vec3(-1, 1,-1) / sqrt(3.0);
+	const vec3 d = vec3(-1,-1, 1) / sqrt(3.0);
+	float z = distance${model.id}(p);
+	return normalize(
+		a * (distance${model.id}(p + a * epsilon) - z) + 
+		b * (distance${model.id}(p + b * epsilon) - z) +
+		c * (distance${model.id}(p + c * epsilon) - z) +
+		d * (distance${model.id}(p + d * epsilon) - z));
+}`;
+
 	else
 		code += `
-		vec3 normal${model.id}(vec3 p) {
-			return normalize(vec3(
- 				distance${model.id}(p + vec3(epsilon, 0, 0)) -
- 				distance${model.id}(p - vec3(epsilon, 0, 0)),
- 				distance${model.id}(p + vec3(0, epsilon, 0)) -
- 				distance${model.id}(p - vec3(0, epsilon, 0)),
-				distance${model.id}(p + vec3(0, 0, epsilon)) -
- 				distance${model.id}(p - vec3(0, 0, epsilon))));
-		}
-		`;
+
+vec3 normal${model.id}(vec3 p) {
+	return normalize(vec3(
+		distance${model.id}(p + vec3(epsilon, 0, 0)) -
+		distance${model.id}(p - vec3(epsilon, 0, 0)),
+		distance${model.id}(p + vec3(0, epsilon, 0)) -
+		distance${model.id}(p - vec3(0, epsilon, 0)),
+		distance${model.id}(p + vec3(0, 0, epsilon)) -
+		distance${model.id}(p - vec3(0, 0, epsilon))));
+}`;
 
 	return code;
 }
@@ -85,241 +89,241 @@ function buildScene(
 		.map(_ => buildModel(_, options))
 		.reduce((a, b) => a + b, "") + `
 		
-		Closest calculateClosest(vec3 position) {
-			Closest closest;
-			float distance;
-		
-			closest.object = 0;
-			closest.distance = MAX_VALUE;` +
+Closest calculateClosest(vec3 position) {
+	Closest closest;
+	float distance;
+
+	closest.object = 0;
+	closest.distance = MAX_VALUE;` +
 
 		scene.models
 			.map((model, i) => `
-			
-			distance = abs(distance${model.id}(position));
-			if (distance < closest.distance) {
-				closest.distance = distance;
-				closest.object = ${model.id};
-			}`)
+	
+	distance = abs(distance${model.id}(position));
+	if (distance < closest.distance) {
+		closest.distance = distance;
+		closest.object = ${model.id};
+	}`)
 			.reduce((a, b) => a + b, "") + `
-			
-			return closest;
-		}
-		
-		vec3 calculateNormal(int object, vec3 position) {` +
+	
+	return closest;
+}
+
+vec3 calculateNormal(int object, vec3 position) {` +
 
 		scene.models
 			.map((model, i) => `
-			
-			if (object == ${model.id})
-				return normal${model.id}(position);`)
+	
+	if (object == ${model.id})
+		return normal${model.id}(position);`)
 			.reduce((a, b) => a + b, "") + `
-			
-			return vec3(0, 0, 0);
-		}
-		
-		Material calculateMaterial(int object, vec3 position, vec3 normal, vec3 direction) {` +
+	
+	return vec3(0, 0, 0);
+}
+
+Material calculateMaterial(int object, vec3 position, vec3 normal, vec3 direction) {` +
 
 		scene.models
 			.map((model, i) => `
-			
-			if (object == ${model.id})
-				return material${model.id}(position, normal, direction);`)
+	
+	if (object == ${model.id})
+		return material${model.id}(position, normal, direction);`)
 			.reduce((a, b) => a + b, "") + `
-			
-			Material material;
-			return material;
-		}`;
+	
+	Material material;
+	return material;
+}`;
 }
 
 export function build(
 	scene: Scene,
 	options: Options): Code {
 	const code = `
-	precision highp float;
+precision highp float;
 
-	uniform sampler2D texture;
-	uniform vec2 resolution;
-	uniform vec2 mouse;
-	uniform bool clicked;
-	uniform float time;
-	varying vec2 uv;
+uniform sampler2D texture;
+uniform vec2 resolution;
+uniform vec2 mouse;
+uniform bool clicked;
+uniform float time;
+varying vec2 uv;
 
-	const float PI = 3.14159;
-	const float MAX_VALUE = 1e5;
+const float PI = 3.14159;
+const float MAX_VALUE = 1e5;
 
-	const float epsilon = ${options.epsilon};
-	const int steps = ${options.steps};
-	const int bounces = ${options.bounces};
-	const int iterations = ${options.iterations};
+const float epsilon = ${options.epsilon};
+const int steps = ${options.steps};
+const int bounces = ${options.bounces};
+const int iterations = ${options.iterations};
 
-	struct Closest {
-		int object;
-		float distance;
-	};
+struct Closest {
+	int object;
+	float distance;
+};
 
-	struct Material {
-		float transmittance;
-		float smoothness;
-		float refraction;
-		float scatter;
-		vec3 color;
-		vec3 emissivity;
-	};
+struct Material {
+	float transmittance;
+	float smoothness;
+	float refraction;
+	float scatter;
+	vec3 color;
+	vec3 emissivity;
+};
 
-	Closest calculateClosest(vec3 position);
-	vec3 calculateNormal(int object, vec3 position);
-	Material calculateMaterial(int object, vec3 position, vec3 normal, vec3 direction);
+Closest calculateClosest(vec3 position);
+vec3 calculateNormal(int object, vec3 position);
+Material calculateMaterial(int object, vec3 position, vec3 normal, vec3 direction);
 
-	vec2 random(int seed) {
-		vec2 s = (uv + vec2(1, 1)) * (1.0 + time + float(seed));
-		return vec2(
-			fract(sin(dot(s.xy, vec2(12.9898, 78.233))) * 43758.5453),
-			fract(cos(dot(s.xy, vec2(4.898, 7.23))) * 23421.631));
-	}
+vec2 random(int seed) {
+	vec2 s = (uv + vec2(1, 1)) * (1.0 + time + float(seed));
+	return vec2(
+		fract(sin(dot(s.xy, vec2(12.9898, 78.233))) * 43758.5453),
+		fract(cos(dot(s.xy, vec2(4.898, 7.23))) * 23421.631));
+}
 
-	vec3 ortho(vec3 v) {
-		return abs(v.x) > abs(v.z) ? vec3(-v.y, v.x, 0.0) : vec3(0.0, -v.z, v.y);
-	}
+vec3 ortho(vec3 v) {
+	return abs(v.x) > abs(v.z) ? vec3(-v.y, v.x, 0.0) : vec3(0.0, -v.z, v.y);
+}
 
-	vec3 calculateSample(vec3 normal, float smoothness, vec2 noise) {
-		vec3 o1 = normalize(ortho(normal));
-		vec3 o2 = normalize(cross(normal, o1));
-		noise.x *= 2.0 * PI;
-		noise.y = sqrt(smoothness + (1.0 - smoothness) * noise.y);
-		float q = sqrt(1.0 - noise.y * noise.y);
-		return q * (cos(noise.x) * o1  + sin(noise.x) * o2) + noise.y * normal;
-	}
+vec3 calculateSample(vec3 normal, float smoothness, vec2 noise) {
+	vec3 o1 = normalize(ortho(normal));
+	vec3 o2 = normalize(cross(normal, o1));
+	noise.x *= 2.0 * PI;
+	noise.y = sqrt(smoothness + (1.0 - smoothness) * noise.y);
+	float q = sqrt(1.0 - noise.y * noise.y);
+	return q * (cos(noise.x) * o1  + sin(noise.x) * o2) + noise.y * normal;
+}
 
-	vec3 sampleSphere(vec2 noise) {
-		noise.x *= 2.0 * PI;
-		noise.y = noise.y * 2.0 - 1.0;
-		float q = sqrt(1.0 - noise.y * noise.y);
-		return vec3(q * cos(noise.x), q * sin(noise.x), noise.y);
-	}
+vec3 sampleSphere(vec2 noise) {
+	noise.x *= 2.0 * PI;
+	noise.y = noise.y * 2.0 - 1.0;
+	float q = sqrt(1.0 - noise.y * noise.y);
+	return vec3(q * cos(noise.x), q * sin(noise.x), noise.y);
+}
 
-	vec3 spherical(vec2 angle) {
-		return vec3(sin(angle.y) * cos(angle.x), cos(angle.y), sin(angle.y) * sin(angle.x));
-	}
+vec3 spherical(vec2 angle) {
+	return vec3(sin(angle.y) * cos(angle.x), cos(angle.y), sin(angle.y) * sin(angle.x));
+}
 
-	void main() {
-		vec3 eye = ${scene.camera.eye};
-		vec3 target = ${scene.camera.target};
-		vec3 up = ${scene.camera.up};
-		float fieldOfView = ${scene.camera.fieldOfView}.x;
-		float aperture = ${scene.camera.aperture}.x;
+void main() {
+	vec3 eye = ${scene.camera.eye};
+	vec3 target = ${scene.camera.target};
+	vec3 up = ${scene.camera.up};
+	float fieldOfView = ${scene.camera.fieldOfView}.x;
+	float aperture = ${scene.camera.aperture}.x;
 
-		vec3 look = normalize(target - eye);
-		up = normalize(up - dot(look, up) * look);
-		vec3 right = cross(look, up);
-		
-		vec3 total;
-
-		for(int iteration = 1; iteration <= iterations; iteration++) {
-			vec2 noise = random(iteration);
+	vec3 look = normalize(target - eye);
+	up = normalize(up - dot(look, up) * look);
+	vec3 right = cross(look, up);
 	
-			vec2 offset = noise.x * aperture * vec2(cos(noise.y * 2.0 * PI), sin(noise.y * 2.0 * PI));
-			vec3 from = eye + offset.x * right + offset.y * up;
-	
-			vec2 angle = (uv * 0.5 + (noise - 0.5) / resolution) * fieldOfView;
-			vec3 screen = vec3(cos(angle.y) * sin(angle.x), sin(angle.y), cos(angle.y) * cos(angle.x));
-			vec3 to = eye + length(target - eye) * (right * screen.x + up * screen.y + look * screen.z);
-	
-			vec3 direction = normalize(to - from);
-	
-			vec3 luminance = vec3(1);
+	vec3 total;
 
-			Material air;
-			air.refraction = ${scene.air.refraction}.x;
-			air.scatter = ${scene.air.scatter}.x;
-			air.emissivity = ${scene.air.emissivity};
-			air.color = ${scene.air.color};
+	for(int iteration = 1; iteration <= iterations; iteration++) {
+		vec2 noise = random(iteration);
 
-			Material current = air;
-	
-			for (int bounce = 1; bounce <= bounces; bounce++) {
-				Closest closest;
-				vec3 position = from;
-				float distance = 0.0;
-	
-				vec2 noise = random(iteration * bounces + bounce);
-	
-				float scatter = -log(noise.y) * current.scatter;
-	
-				for (int step = 1; step <= steps; step++) {
-					closest = calculateClosest(position);
+		vec2 offset = noise.x * aperture * vec2(cos(noise.y * 2.0 * PI), sin(noise.y * 2.0 * PI));
+		vec3 from = eye + offset.x * right + offset.y * up;
 
-					if (closest.distance < epsilon)
-						break;
-	
-					distance = distance + closest.distance;
-					distance = min(distance, scatter);
-					position = from + direction * distance;
+		vec2 angle = (uv * 0.5 + (noise - 0.5) / resolution) * fieldOfView;
+		vec3 screen = vec3(cos(angle.y) * sin(angle.x), sin(angle.y), cos(angle.y) * cos(angle.x));
+		vec3 to = eye + length(target - eye) * (right * screen.x + up * screen.y + look * screen.z);
 
-					if (distance == scatter)
-						break;
-				}
+		vec3 direction = normalize(to - from);
 
-				if (closest.object == 0)
+		vec3 luminance = vec3(1);
+
+		Material air;
+		air.refraction = ${scene.air.refraction}.x;
+		air.scatter = ${scene.air.scatter}.x;
+		air.emissivity = ${scene.air.emissivity};
+		air.color = ${scene.air.color};
+
+		Material current = air;
+
+		for (int bounce = 1; bounce <= bounces; bounce++) {
+			Closest closest;
+			vec3 position = from;
+			float distance = 0.0;
+
+			vec2 noise = random(iteration * bounces + bounce);
+
+			float scatter = -log(noise.y) * current.scatter;
+
+			for (int step = 1; step <= steps; step++) {
+				closest = calculateClosest(position);
+
+				if (closest.distance < epsilon)
 					break;
-	
-				if (distance == scatter) {
-					from = position;
-					direction = sampleSphere(noise);
-					total += luminance * current.emissivity;
-					luminance *= current.color;
+
+				distance = distance + closest.distance;
+				distance = min(distance, scatter);
+				position = from + direction * distance;
+
+				if (distance == scatter)
+					break;
+			}
+
+			if (closest.object == 0)
+				break;
+
+			if (distance == scatter) {
+				from = position;
+				direction = sampleSphere(noise);
+				total += luminance * current.emissivity;
+				luminance *= current.color;
+				continue;
+			}
+
+			vec3 normal = calculateNormal(closest.object, position);
+
+			Material material = calculateMaterial(closest.object, position, normal, direction);
+
+			total += luminance * material.emissivity;
+
+			if (material.color == vec3(0))
+				break;
+
+			bool backface = dot(direction, normal) > 0.0; 
+			if (backface)
+				normal = -normal; 
+
+			normal = calculateSample(normal, material.smoothness, noise);
+			
+			if (noise.y < material.transmittance) {
+				float eta;
+				if (!backface)
+					eta = current.refraction / material.refraction;
+				else
+					eta = material.refraction / air.refraction;
+			
+				vec3 refracted = refract(direction, normal, eta);
+				if (refracted != vec3(0)) {
+					from = position - 5.0 * epsilon * normal;
+					direction = refracted;
+					if (!backface)
+						current = material;
+					else
+						current = air;
 					continue;
 				}
-	
-				vec3 normal = calculateNormal(closest.object, position);
-
-				Material material = calculateMaterial(closest.object, position, normal, direction);
-
-				total += luminance * material.emissivity;
-
-				if (material.color == vec3(0))
-					break;
-	
-				bool backface = dot(direction, normal) > 0.0; 
-				if (backface)
-					normal = -normal; 
-	
-				normal = calculateSample(normal, material.smoothness, noise);
-				
-				if (noise.y < material.transmittance) {
-					float eta;
-					if (!backface)
-						eta = current.refraction / material.refraction;
-					else
-						eta = material.refraction / air.refraction;
-				
-					vec3 refracted = refract(direction, normal, eta);
-					if (refracted != vec3(0)) {
-						from = position - 5.0 * epsilon * normal;
-						direction = refracted;
-						if (!backface)
-							current = material;
-					 	else
-							current = air;
-						continue;
-					}
-				}
-
-				luminance *= material.color;				
-				from = position + 5.0 * epsilon * normal;
-				direction = reflect(direction, normal);
 			}
+
+			luminance *= material.color;				
+			from = position + 5.0 * epsilon * normal;
+			direction = reflect(direction, normal);
 		}
- 
-		vec4 original = texture2D(texture, uv * 0.5 - 0.5);
+	}
+
+	vec4 original = texture2D(texture, uv * 0.5 - 0.5);
+	
+	if (clicked) 
+		original *= 0.5;
+
+	original *= ${options.memory.toFixed(10)}; 
 		
-		if (clicked) 
-			original *= 0.5;
+	gl_FragColor = original + vec4(total, iterations);
 
-		original *= ${options.memory.toFixed(10)}; 
-			
-		gl_FragColor = original + vec4(total, iterations);
-
-	}` + buildScene(scene, options);
+}` + buildScene(scene, options);
 
 	console.log(code);
 
--- src/shape.ts
+++ src/shape.ts
@@ -25,7 +25,7 @@ export function unitSphere(): Shape {
 	return shape(`return length(p) - 0.5;`);
 }
 
-export function smoothBox(dimensions: Expression, radius: Expression) {
+export function smoothBox(dimensions: Expression, radius: Expression): Shape {
 	return mirror(value(1, 0, 0),
 		mirror(value(0, 1, 0),
 			mirror(value(0, 0, 1),
@@ -35,18 +35,18 @@ export function smoothBox(dimensions: Expression, radius: Expression) {
 							unitSphere()))))));
 }
 
-function unit() {
+function unit(): Shape {
 	return shape(`return -MAX_VALUE;`);
 }
 
-function unitShape(normals: Expression[]) {
+function unitShape(normals: Expression[]): Shape {
 	return normals.reduce((s, n) =>
 		intersection(s,
 			plane(n, value(-0.5))),
 		unit());
 }
 
-export function unitTetrahedron() {
+export function unitTetrahedron(): Shape {
 	let l = Math.sqrt(3);
 	return unitShape([
 		value(-1 / l, -1 / l, -1 / l),
@@ -56,7 +56,7 @@ export function unitTetrahedron() {
 	]);
 }
 
-export function unitCube() {
+export function unitCube(): Shape {
 	return unitShape([
 		value(1, 0, 0),
 		value(-1, 0, 0),
@@ -67,7 +67,7 @@ export function unitCube() {
 	]);
 }
 
-export function unitOctohedron() {
+export function unitOctohedron(): Shape {
 	let l = Math.sqrt(3);
 	return unitShape([
 		value(1 / l, 1 / l, 1 / l),
@@ -81,7 +81,7 @@ export function unitOctohedron() {
 	]);
 }
 
-export function unitDodecahedron() {
+export function unitDodecahedron(): Shape {
 	let phi = 0.5 * (1 + Math.sqrt(5));
 	let l = Math.sqrt(phi * phi + 1);
 	return unitShape([
@@ -106,145 +106,146 @@ export function cylinder(): Shape {
 
 export function torus(): Shape {
 	return shape(`
-		vec2 q = vec2(length(p.xz) - 0.5, p.y);
-  		return length(q) - 0.1;`);
+	vec2 q = vec2(length(p.xz) - 0.5, p.y);
+	return length(q) - 0.1;
+`);
 }
 
-export function plane(normal: Expression, offset: Expression) {
+export function plane(normal: Expression, offset: Expression): Shape {
 	return shape(`return dot(p, ${normal}) + ${offset}.x;`);
 }
 
-export function translate(x: Expression, a: Shape) {
+export function translate(x: Expression, a: Shape): Shape {
 	return shape(`return ${a.call(`p - ${x}`)};`,
 		[a]);
 }
 
-export function scale(x: Expression, a: Shape) {
-	return shape(`return ${a.call(`p / ${x}.x`)} * ${x}.x;`,
+export function scale(x: Expression, a: Shape): Shape {
+	return shape(`
+	float x = ${x}.x;
+	return ${a.call(`p / x`)} * x;`,
 		[a]);
 }
 
-export function max(a: Shape) {
+export function max(a: Shape): Shape {
 	return shape(`return ${a.call(`max(p, 0.0)`)};`,
 		[a]);
 }
 
-export function stretch(x: Expression, a: Shape) {
+export function stretch(x: Expression, a: Shape): Shape {
 	return shape(`return ${a.call(`p / ${x}`)}; `,
 		[a]);
 }
 
-export function repeat(x: Expression, a: Shape) {
+export function repeat(x: Expression, a: Shape): Shape {
 	return shape(`return ${a.call(`mod(p - ${x} * 0.5, ${x}) - ${x} * 0.5`)};`,
 		[a]);
 }
 
-export function union(a: Shape, b: Shape) {
+export function union(a: Shape, b: Shape): Shape {
 	return shape(`return min(${a.call("p")}, ${b.call("p")});`,
 		[a, b]);
 }
 
-export function intersection(a: Shape, b: Shape) {
+export function intersection(a: Shape, b: Shape): Shape {
 	return shape(`return max(${a.call("p")}, ${b.call("p")});`,
 		[a, b]);
 }
 
-export function difference(a: Shape, b: Shape) {
+export function difference(a: Shape, b: Shape): Shape {
 	return shape(`return max(${a.call("p")}, -${b.call("p")});`,
 		[a, b]);
 }
 
-export function blend(k: Expression, a: Shape, b: Shape) {
+export function blend(k: Expression, a: Shape, b: Shape): Shape {
 	return shape(`
-		float a = ${a.call("p")};
-		float b = ${b.call("p")};
-		float k = ${k}.x;
-		float h = clamp(0.5 + 0.5 * (b - a) / k, 0.0, 1.0);
-		return mix(b, a, h) - k * h * (1.0 - h);`,
+	float a = ${a.call("p")};
+	float b = ${b.call("p")};
+	float k = ${k}.x;
+	float h = clamp(0.5 + 0.5 * (b - a) / k, 0.0, 1.0);
+	return mix(b, a, h) - k * h * (1.0 - h);`,
 		[a, b]);
 }
 
-export function expand(k: Expression, a: Shape) {
-	return shape(`
-		return ${a.call(`p`)} - ${k}.x;`,
+export function expand(k: Expression, a: Shape): Shape {
+	return shape(`return ${a.call(`p`)} - ${k}.x;`,
 		[a]);
 }
 
-export function twistX(x: Expression, a: Shape) {
+export function twistX(x: Expression, a: Shape): Shape {
 	return rotateZ(`vec3(p.x * ${x}.x)`, a);
 }
 
-export function twistY(x: Expression, a: Shape) {
+export function twistY(x: Expression, a: Shape): Shape {
 	return rotateZ(`vec3(p.y * ${x}.x)`, a);
 }
 
-export function twistZ(x: Expression, a: Shape) {
+export function twistZ(x: Expression, a: Shape): Shape {
 	return rotateZ(`vec3(p.z * ${x}.x)`, a);
 }
 
-export function wrapX(a: Shape) {
+export function wrapX(a: Shape): Shape {
 	return shape(`
-		float q = length(p.yz);
-		return ${a.call(`vec3(p.x, q, asin(p.z / q))`)};`,
+	float q = length(p.yz);
+	return ${a.call(`vec3(p.x, q, asin(p.z / q))`)};`,
+	[a]);
+}
+
+export function rotateX(x: Expression, a: Shape): Shape {
+	return shape(`
+	float c = cos(${x}.x);
+	float s = sin(${x}.x);
+	mat3 m = mat3(1, 0, 0, 0, c, s, 0, -s, c);
+	return ${a.call(`m * p`)};`,
 		[a]);
 }
 
-export function rotateX(x: Expression, a: Shape) {
+export function rotateY(x: Expression, a: Shape): Shape {
 	return shape(`
-		float c = cos(${x}.x);
-		float s = sin(${x}.x);
-		mat3 m = mat3(1, 0, 0, 0, c, s, 0, -s, c);
-		return ${a.call(`m * p`)};`,
+	float c = cos(${x}.x);
+	float s = sin(${x}.x);
+	mat3 m = mat3(c, 0, -s, 0, 1, 0, s, 0, c);
+	return ${a.call(`m * p`)};`,
 		[a]);
 }
 
-export function rotateY(x: Expression, a: Shape) {
+export function rotateZ(x: Expression, a: Shape): Shape {
 	return shape(`
-		float c = cos(${x}.x);
-		float s = sin(${x}.x);
-		mat3 m = mat3(c, 0, -s, 0, 1, 0, s, 0, c);
-		return ${a.call(`m * p`)};`,
+	float c = cos(${x}.x);
+	float s = sin(${x}.x);
+	mat3 m = mat3(c, s, 0, -s, c, 0, 0, 0, 1);
+	return ${a.call(`m * p`)};`,
 		[a]);
 }
 
-export function rotateZ(x: Expression, a: Shape) {
+export function rotate(axis: Expression, x: Expression, a: Shape): Shape {
 	return shape(`
-		float c = cos(${x}.x);
-		float s = sin(${x}.x);
-		mat3 m = mat3(c, s, 0, -s, c, 0, 0, 0, 1);
-		return ${a.call(`m * p`)};`,
+	vec3 u = normalize(${axis});
+	float c = cos(${x}.x);
+	float s = sin(${x}.x);
+	mat3 m = mat3(
+		c + u.x * u.x * (1.0 - c), 
+		u.x * u.y * (1.0 - c) - u.z * s,
+		u.x * u.z * (1.0 - c) + u.y * s,
+		u.y * u.x * (1.0 - c) + u.z * s,
+		c + u.y * u.y * (1.0 - c),
+		u.x * u.y * (1.0 - c) - u.x * s,
+		u.z * u.x * (1.0 - c) - u.y * s,
+		u.z * u.y * (1.0 - c) + u.x * s,
+		c +  u.z * u.z * (1.0 - c));
+	return ${a.call(`m * p`)};`,
 		[a]);
 }
 
-export function rotate(axis: Expression, x: Expression, a: Shape) {
+export function mirror(n: Expression, a: Shape): Shape {
 	return shape(`
-		vec3 u = normalize(${axis});
-		float c = cos(${x}.x);
-		float s = sin(${x}.x);
-		mat3 m = mat3(
-			c + u.x * u.x * (1.0 - c), 
-			u.x * u.y * (1.0 - c) - u.z * s,
-			u.x * u.z * (1.0 - c) + u.y * s,
-			u.y * u.x * (1.0 - c) + u.z * s,
-			c + u.y * u.y * (1.0 - c),
-			u.x * u.y * (1.0 - c) - u.x * s,
-			u.z * u.x * (1.0 - c) - u.y * s,
-			u.z * u.y * (1.0 - c) + u.x * s,
-			c +  u.z * u.z * (1.0 - c));
-		return ${a.call(`m * p`)};`,
+	vec3 n = ${n};
+	return ${a.call(`p - 2.0 * min(0.0, dot(p, n)) * n`)};`,
 		[a]);
 }
 
-export function mirror(n: Expression, a: Shape) {
-	return shape(`
-		p -= 2.0 * min(0.0, dot(p, ${n})) * ${n};
-		return ${a.call(`p`)};`,
-		[a]);
-}
-
-export function sierpinski(iterations: number = 5, a: Shape = unitTetrahedron()) {
+export function sierpinski(iterations: number = 5, a: Shape = unitTetrahedron()): Shape {
 	let l = Math.sqrt(2);
-	let shape = a;
 	return Array(iterations)
 		.fill(0)
 		.reduce((shape, i) =>
@@ -253,36 +254,54 @@ export function sierpinski(iterations: number = 5, a: Shape = unitTetrahedron())
 					mirror(value(1 / l, 0, 1 / l),
 						scale(value(0.5),
 							translate(value(0.5 * Math.sqrt(3)),
-								shape))))));
+								shape))))), a);
 }
 
-export function mandelbulb(iterations: number = 5) {
+export function test(iterations: number = 5, a: Shape = intersection(
+	scale(value(0.2),
+		cylinder()),
+	unitSphere())): Shape {
+	let l = Math.sqrt(2);
+	return Array(iterations)
+		.fill(0)
+		.reduce((shape, i) =>
+			union(
+				shape,
+				mirror(value(1 / l, 0, 1 / l),
+					mirror(value(1 / l, 0, -1 / l),
+						translate(value(0, 0.5, 0),
+							rotate(value(0, 0, 1), value(-30 / 180 * Math.PI),
+								translate(value(0, 0.5, 0),
+									scale(value(0.8), shape))))))), a);
+}
+
+export function mandelbulb(iterations: number = 5): Shape {
 	return shape(`
-		vec3 z = p;
-		float d = 0.0;
-		float q = 8.0;
-		float s = 1.0;
-		
-		for(int i = 1; i <= ${iterations}; i++) {
-		
-			float r = length(z);
-	
-			if(r > 2.0) {
-				d = 0.5 * log(r) * r / s;
-				continue;
-			}
-			
-			float th = atan(length(z.xy), z.z);
-			float phi = atan(z.y, z.x);
-			float rado = pow(r, 8.0);
-			s = pow(r, 7.0) * 7.0 * s + 1.0;
-			
-			float sint = sin(th * q);
-			z.x = rado * sint * cos(phi * q);
-			z.y = rado * sint * sin(phi * q);
-			z.z = rado * cos(th * q) ;
-			z += p;
+	vec3 z = p;
+	float d = 0.0;
+	float q = 8.0;
+	float s = 1.0;
+
+	for(int i = 1; i <= ${iterations}; i++) {
+
+		float r = length(z);
+
+		if(r > 2.0) {
+			d = 0.5 * log(r) * r / s;
+			continue;
 		}
 		
-		return d;`);
+		float th = atan(length(z.xy), z.z);
+		float phi = atan(z.y, z.x);
+		float rado = pow(r, 8.0);
+		s = pow(r, 7.0) * 7.0 * s + 1.0;
+		
+		float sint = sin(th * q);
+		z.x = rado * sint * cos(phi * q);
+		z.y = rado * sint * sin(phi * q);
+		z.z = rado * cos(th * q) ;
+		z += p;
+	}
+
+	return d;`);
 }
