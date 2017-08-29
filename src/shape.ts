5b04fb465492cdf120bd337a0bd191a7fc8f49a3
Graham <graham.gibbons@gmail.com>
2017-08-29 09:51:53 -0700

    Add skull model

--- src/shape.ts
+++ src/shape.ts
@@ -4,7 +4,7 @@ export interface Shape {
 	readonly call: (position: Expression) => Expression,
 }
 
-function shape(call: (position: Expression) => Expression): Shape {
+export function shape(call: (position: Expression) => Expression): Shape {
 	return {
 		call: call
 	};
@@ -115,6 +115,10 @@ export function scale(x: Expression, a: Shape): Shape {
 	});
 }
 
+export function spheroid(x: (p: Expression) => Expression) {
+	return shape(p => expression(`length(${p}) - ${x(p)}`));
+}
+
 export function max(a: Shape): Shape {
 	return shape(p =>
 		a.call(expression(`max(${p}, 0.0)`)));
@@ -154,13 +158,29 @@ export function difference(a: Shape, b: Shape): Shape {
 	});
 }
 
-export function blend(k: Expression, a: Shape, b: Shape): Shape {
-	return shape(p => {
-		let da = a.call(p);
-		let db = b.call(p);
-		let h = expression(`clamp(0.5 + 0.5 * (${db} - ${da}) / ${k}, 0.0, 1.0)`);
-		return expression(`mix(${db}, ${da}, ${h}) - ${k} * ${h} * (1.0 - ${h})`);
-	});
+function smoothMin(k: Expression, a: Expression, b: Expression): Expression {
+	let h = expression(`clamp(0.5 + 0.5 * (${b} - ${a}) / ${k}, 0.0, 1.0)`);
+	return expression(`mix(${b}, ${a}, ${h}) - ${k} * ${h} * (1.0 - ${h})`);
+}
+
+function smoothMax(k: Expression, a: Expression, b: Expression): Expression {
+	let h = expression(`clamp(0.5 - 0.5 * (${b} - ${a}) / ${k}, 0.0, 1.0)`);
+	return expression(`mix(${b}, ${a}, ${h}) + ${k} * ${h} * (1.0 - ${h})`);
+}
+
+export function smoothUnion(k: Expression, a: Shape, b: Shape): Shape {
+	return shape(p =>
+		smoothMin(k, a.call(p), b.call(p)));
+}
+
+export function smoothIntersection(k: Expression, a: Shape, b: Shape): Shape {
+	return shape(p =>
+		smoothMax(k, a.call(p), b.call(p)));
+}
+
+export function smoothDifference(k: Expression, a: Shape, b: Shape): Shape {
+	return shape(p =>
+		smoothMax(k, a.call(p), expression(`${b.call(p)} * -1.0`)));
 }
 
 export function expand(k: Expression, a: Shape): Shape {
@@ -225,14 +245,20 @@ export function rotate(axis: Expression, x: Expression, a: Shape): Shape {
 
 export function wrapX(a: Shape): Shape {
 	return shape(p => {
-		let c = expression(`length(p.yz)`);
-		return a.call(expression(`${p}.x, ${c}.x, asin(${p}.z / ${c}.x)`));
+		let c = expression(`length(${p}.yz)`);
+		let q = expression(`1, 1, max(0.01, abs(${p}.z))`);
+		//let q = value(1);
+		return expression(`${a.call(expression(`${p}.x, asin(${p}.y / ${c}.x), ${c}.x`))} * ${minNorm(q)}`);
 	});
 }
 
 export function mirror(n: Expression, a: Shape): Shape {
 	return shape(p =>
 		a.call(expression(`${p} - 2.0 * min(0.0, dot(${p}, ${n})) * ${n}`)));
+}
+
+export function offset(x: (p: Expression) => Expression, a: Shape): Shape {
+	return shape(p => a.call(expression(`${p} - ${x(p)}`)));
 }
 
 export function smoothBox(dimensions: Expression, radius: Expression): Shape {
@@ -243,6 +269,13 @@ export function smoothBox(dimensions: Expression, radius: Expression): Shape {
 					max(
 						scale(radius,
 							sphere()))))));
+}
+
+export function box(dimensions: Expression): Shape {
+	return shape(p => {
+		const d = expression(`abs(${p}) - ${dimensions}`);
+		return expression(`max(min(${d}.x, min(${d}.y, ${d}.z)), 0.0) + length(max(${d}, 0.0))`);
+	});
 }
 
 export function sierpinski(iterations: number = 5, a: Shape = tetrahedron()): Shape {
@@ -267,7 +300,7 @@ export function tree(iterations: number = 8): Shape {
 	return Array(iterations)
 		.fill(0)
 		.reduce((shape, _, i) =>
-			blend(
+			smoothUnion(
 				value(0.15 * Math.pow(factor, i)),
 				shape,
 				mirror(value(1 / Math.sqrt(2), 0, 1 / Math.sqrt(2)),
@@ -327,4 +360,161 @@ export function truchet(): Shape {
 			rotateX(value(Math.PI), base),
 			rotateX(value(3 * Math.PI / 2), base)
 		]));
+}
+
+export function skull(): Shape {
+	let skull =
+		translate(value(0, 0.05, 0),
+			spheroid(p => expression(`0.333 * cos(cos(${p}.y * 11.0 + 0.55) * ${p}.z * 2.3)`)));
+
+	const globeFront = translate(
+		value(0.1, 0.23, 0),
+		scale(value(0.574), sphere()));
+	skull = smoothUnion(value(0.09), skull, globeFront);
+
+	const globeBack = translate(
+		value(-0.1, 0.24, 0),
+		scale(value(0.574), sphere()));
+	skull = smoothUnion(value(0.09), skull, globeBack);
+
+	const eyeBrow = translate(value(0.24, 0.07, 0.1),
+		spheroid(p => expression(`0.126 * cos(${p}.y * 7.0 + 0.49)`)));
+	skull = smoothUnion(value(0.02), skull, eyeBrow);
+
+	const lateralHole = translate(value(0.15, -0.01, 0.31),
+		spheroid(p => expression(`0.098 * cos(${p}.x * 0.59 + 0.089)`)));
+	skull = smoothDifference(value(0.02), skull, lateralHole);
+
+	const cheekBone = translate(value(0.22, -0.13, 0.18),
+		scale(value(0.077), sphere()));
+	skull = smoothUnion(value(0.04), skull, cheekBone);
+
+	let inside = translate(value(0, 0.05, 0),
+		spheroid(p => expression(`0.315 * cos(cos(${p}.y * 11.0 + 0.55) * ${p}.z * 2.3)`)))
+	inside = smoothUnion(value(0.02),
+		inside,
+		translate(value(0.10, 0.23, 0),
+			scale(value(0.511),
+				sphere())));
+	inside = smoothUnion(value(0.02),
+		inside, translate(value(-0.1, 0.24, 0),
+			scale(value(0.511),
+				sphere())));
+	inside = smoothUnion(value(0.02),
+		inside,
+		translate(value(0, 0.24, 0),
+			scale(value(0.511),
+				sphere())));
+	skull = smoothDifference(value(0.02), skull, inside);
+
+	const eyeBall = translate(value(0.32, -0.04, 0.140),
+		spheroid(p => expression(`0.098 * cos(${p}.y * 10.0 - 0.04)`)))
+	skull = smoothDifference(value(0.03), skull, eyeBall);
+
+	let nose = translate(
+		value(0.22, -0.05, 0),
+		spheroid(p => expression(`0.123 * cos(sin(${p}.y * 22.0 - 1.1) * ${p}.z * 24.0)`)));
+	nose = smoothDifference(value(0.02),
+		nose,
+		translate(value(0.32, -0.04, 0.140),
+			spheroid(p => expression(`0.123 * cos(${p}.y * 10.0 - 0.4)`))));
+	nose = smoothDifference(value(0.02),
+		nose,
+		translate(value(0, 0.05, 0),
+			spheroid(p => expression(`0.32 * cos(cos(${p}.y * 11.0 + 0.5) * ${p}.z * 2.3)`))));
+	skull = smoothUnion(value(0.015), skull, nose);
+
+	const noseInside = translate(value(0.238, -0.09, 0),
+		spheroid(p => expression(`0.11 * cos(sin(${p}.y * 18.0 - 1.62) * ${p}.z * 29.0)`)));
+	skull = smoothDifference(value(0.002),
+		skull,
+		noseInside);
+
+	const cut = translate(value(-0.15, -0.97, 0),
+		scale(value(1.75),
+			sphere()));
+	skull = smoothDifference(value(0.01), skull, cut);
+
+	let upperJaw = translate(value(0.13, -0.26, 0),
+		scale(value(0.315),
+			sphere()));
+	upperJaw = smoothDifference(value(0.01),
+		upperJaw,
+		translate(value(0.125, -0.3, 0),
+			scale(value(0.28),
+				sphere())));
+	upperJaw = smoothDifference(value(0.03),
+		upperJaw,
+		translate(value(-0.2, -0.1, 0),
+			scale(value(0.63),
+				sphere())));
+	upperJaw = smoothDifference(value(0.03),
+		upperJaw,
+		translate(value(0.13, -0.543, 0),
+			scale(value(0.63),
+				sphere())));
+	upperJaw = difference(
+		upperJaw,
+		translate(value(0, 0.02, 0),
+			spheroid(p => expression(`0.315 * cos(cos(${p}.y * 11.0 + 0.22) * ${p}.z * 2.3)`))));
+	skull = smoothUnion(value(0.04), skull, upperJaw);
+
+	let lowerJaw = translate(value(0.1, -0.32, 0),
+		scale(value(0.301),
+			sphere()));
+	lowerJaw = smoothDifference(value(0.02),
+		lowerJaw,
+		translate(value(0.1, -0.32, 0),
+			scale(value(0.259),
+				sphere())));
+	lowerJaw = smoothDifference(value(0.02),
+		lowerJaw,
+		translate(value(0.1, -0.034, 0),
+			scale(value(0.721),
+				sphere())));
+	lowerJaw = smoothDifference(value(0.02),
+		lowerJaw,
+		translate(value(0, -0.4, 0),
+			scale(value(0.245),
+				sphere())));
+	lowerJaw = smoothUnion(value(0.13),
+		lowerJaw,
+		offset(p => expression(`0.04 - 0.03 * cos(${p}.y * 20.2), -0.23, 0.27 + sin(${p}.y) * 0.27`),
+			box(value(0.03, 0.12, 0.014))));
+	lowerJaw = difference(
+		lowerJaw,
+		translate(value(0, 0.153, 0.2),
+			scale(value(0.595),
+				sphere())));
+	lowerJaw = smoothUnion(
+		value(0.07),
+		lowerJaw,
+		translate(value(0.2, -0.45, 0.05),
+			scale(value(0.035),
+				sphere())));
+	skull = smoothUnion(value(0.02), skull, lowerJaw);
+
+	let teeth = translate(value(0.26, -0.29, 0.018), scale(value(0.0371), sphere()));
+	teeth = union(teeth, translate(value(0.25, -0.29, 0.05), scale(value(0.035), sphere())));;
+	teeth = union(teeth, translate(value(0.235, -0.29, 0.08), scale(value(0.035), sphere())));;
+	teeth = union(teeth, translate(value(0.215, -0.28, 0.1), scale(value(0.035), sphere())));;
+	teeth = difference(teeth, translate(value(0.16, -0.35, 0), scale(value(0.231), sphere())));;
+	teeth = union(teeth, translate(value(0.18, -0.28, 0.115), scale(value(0.035), sphere())));;
+	teeth = union(teeth, translate(value(0.14, -0.28, 0.115), scale(value(0.042), sphere())));;
+	teeth = union(teeth, translate(value(0.11, -0.28, 0.115), scale(value(0.042), sphere())));;
+	teeth = union(teeth, translate(value(0.08, -0.28, 0.115), scale(value(0.042), sphere())));;
+	skull = smoothUnion(value(0.03), skull, teeth);
+
+	teeth = translate(value(0.23, -0.34, 0.018), scale(value(0.0371), sphere()));
+	teeth = union(teeth, translate(value(0.22, -0.34, 0.048), scale(value(0.0353), sphere())));;
+	teeth = union(teeth, translate(value(0.20, -0.34, 0.078), scale(value(0.0353), sphere())));;
+	teeth = union(teeth, translate(value(0.17, -0.35, 0.098), scale(value(0.0353), sphere())));;
+	teeth = union(teeth, translate(value(0.14, -0.35, 0.11), scale(value(0.0353), sphere())));;
+	teeth = union(teeth, translate(value(0.11, -0.35, 0.11), scale(value(0.0353), sphere())));;
+	teeth = union(teeth, translate(value(0.08, -0.35, 0.11), scale(value(0.0353), sphere())));;
+	skull = smoothUnion(value(0.025), skull, teeth);
+
+	skull = mirror(value(0, 0, 1), skull);
+
+	return skull;
 }
